0ef2a622c22f578cb988476f3a5eefe807e42d3f
Ahmed Mohamed <118309402+amedmohmed925@users.noreply.github.com>
2024-05-29 12:29:23 +0200

    Add some icons

--- index.js
+++ index.js
@@ -1,106 +1,100 @@
-let  title = document.getElementById("title")
-let  price = document.getElementById("price")
-let  taxes = document.getElementById("taxes")
-let  ads = document.getElementById("ads")
-let  discount = document.getElementById("discount")
-let  total = document.getElementById("total")
-let  count = document.getElementById("count")
-let  category = document.getElementById("category")
-let  create = document.getElementById("create")
-let error = document.getElementById("error")
-let mood= 'create';
+let title = document.getElementById("title");
+let price = document.getElementById("price");
+let taxes = document.getElementById("taxes");
+let ads = document.getElementById("ads");
+let discount = document.getElementById("discount");
+let total = document.getElementById("total");
+let count = document.getElementById("count");
+let category = document.getElementById("category");
+let create = document.getElementById("create");
+let error = document.getElementById("error");
+let mood = "create";
 let index;
- function closeError() {
-    error.style.display =" none"
- }
+function closeError() {
+  error.style.display = " none";
+}
 // get total
-console.log(ads)
+console.log(ads);
 function getTotal() {
-    if (price.value != '') {
-        let result = (+price.value + +taxes.value + +ads.value ) - +discount.value
-        total.innerHTML = result
-        total.style.backgroundColor ="#017aff"
-        total.style.color ="white"
-
-    } else {
-        total.style.backgroundColor ="red"
-        total.style.color ="white"
-        total.innerHTML = ''
-
-    }
+  if (price.value != "") {
+    let result = +price.value + +taxes.value + +ads.value - +discount.value;
+    total.innerHTML = result;
+    total.style.backgroundColor = "#017aff";
+    total.style.color = "white";
+  } else {
+    total.style.backgroundColor = "red";
+    total.style.color = "white";
+    total.innerHTML = "";
+  }
 }
 
 // creat product
 let dataPro;
 
 if (localStorage.dataPro != null) {
-    dataPro = JSON.parse(localStorage.dataPro)
+  dataPro = JSON.parse(localStorage.dataPro);
 } else {
-    dataPro = []
+  dataPro = [];
 }
 create.onclick = function () {
-   
-        let newProduct = {
-            title:title.value.toLowerCase(),
-            price:price.value,
-            taxes:taxes.value,
-            ads:ads.value,
-            total:total.innerHTML,
-            discount:discount.value,
-            count:count.value,
-            category:category.value.toLowerCase(),
+  let newProduct = {
+    title: title.value.toLowerCase(),
+    price: price.value,
+    taxes: taxes.value,
+    ads: ads.value,
+    total: total.innerHTML,
+    discount: discount.value,
+    count: count.value,
+    category: category.value.toLowerCase(),
+  };
+  let titlePage = document.getElementById("titlePage")
+  
+  // count
+  if (title.value != "" && price.value != "" && category.value != "") {
+    if (mood === "create") {
+      if (newProduct.count > 1) {
+        for (let i = 0; i < newProduct.count; i++) {
+          dataPro.push(newProduct);
         }
-        // count
-        if (title.value != '' && price.value != '' && category.value != '') {
-            if (mood === 'create'){
-            if (newProduct.count > 1) {
-                for (let i = 0; i < newProduct.count; i++) {
-                    dataPro.push( newProduct)
+        titlePage.innerHTML = "Product | Create";
+      } else {
+        dataPro.push(newProduct);
+      }
+    } else {
+      dataPro[index] = newProduct;
+      create.innerHTML = "Create";
+      count.style.display = "block";
+    }
 
-                }
-            }   else {
-                dataPro.push( newProduct)
+    clearDta();
+  } else {
+    error.style.display = "block";
+  }
 
-            }
+  localStorage.setItem("dataPro", JSON.stringify(dataPro));
 
-        } else{
-            dataPro[index] = newProduct
-            create.innerHTML="Create"
-            count.style.display="block"
-            
-        } 
-        
-        clearDta()
-        } else{
-            error.style.display='block'
-
-        }
-              
-        localStorage.setItem('dataPro',JSON.stringify(dataPro))
-        
-        readData()
-        getTotal()
-
-}
+  readData();
+  getTotal();
+};
 // clear data
 function clearDta() {
-            title.value = ''
-            price.value = ''
-            taxes.value = ''
-            ads.value = ''
-            total.innerHTML = ''
-            discount.value = ''
-            count.value = ''
-            category.value = ''
+  title.value = "";
+  price.value = "";
+  taxes.value = "";
+  ads.value = "";
+  total.innerHTML = "";
+  discount.value = "";
+  count.value = "";
+  category.value = "";
 }
 // readData
 
 function readData() {
-    let table = '';
-    for (let i = 0; i < dataPro.length; i++) {
-        table += `
+  let table = "";
+  for (let i = 0; i < dataPro.length; i++) {
+    table += `
         <tr>
-                        <td>${i+1}</td>
+                        <td>${i + 1}</td>
                         <td>${dataPro[i].title}</td>
                         <td>${dataPro[i].price}</td>
                         <td>${dataPro[i].taxes}</td>
@@ -108,78 +102,89 @@ function readData() {
                         <td>${dataPro[i].discount}</td>
                         <td>${dataPro[i].total}$</td>
                         <td>${dataPro[i].category}</td>
-                       <td><button onClick="updatePro(${i})">Update</button></td> 
-                       <td><button onClick="deleteData( ${i} )">Delete</button></td> 
+                       <td><button class="updatePtn" onClick="updatePro(${i})"> <i class="fas fa-edit"></i></button></td> 
+                       <td><button class="deletePtn" onClick="deleteData( ${i} )"> <i class="fas fa-trash"></i></button></td> 
 
                     </tr>
-        `
-    }
-    document.getElementById('tbody').innerHTML = table
-    let deleteAllptn = document.getElementById("deleteAll")
-    if (dataPro.length > 0) {
-        deleteAllptn.innerHTML=`<button onClick="deleteAll()">Delete All( ${dataPro.length})</button>`
-    } else {
-        deleteAllptn.innerHTML=''
-    }
+        `;
+  }
+  document.getElementById("tbody").innerHTML = table;
+  let deleteAllptn = document.getElementById("deleteAll");
+  if (dataPro.length > 0) {
+    deleteAllptn.innerHTML = `<button onClick="deleteAll()">Delete All( ${dataPro.length})</button>`;
+  } else {
+    deleteAllptn.innerHTML = "";
+  }
 }
-readData()
+readData();
 
 // delete
 
 function deleteData(i) {
-    dataPro.splice(i,1)
-    localStorage.setItem('dataPro',JSON.stringify(dataPro))
-    readData()
+  dataPro.splice(i, 1);
+  localStorage.setItem("dataPro", JSON.stringify(dataPro));
+  titlePage.innerHTML = "Product | Delete";
+  readData();
 }
 function deleteAll() {
-    localStorage.clear()
-    dataPro.splice(0)
-    readData()
+  localStorage.clear();
+  dataPro.splice(0);
+  titlePage.innerHTML = "Product | Delete All";
+  readData();
 }
 
 // update
 function updatePro(i) {
-    title.value = dataPro[i].title;
-    price.value = dataPro[i].price;
-    taxes.value = dataPro[i].taxes;
-    ads.value = dataPro[i].ads;
-    discount.value = dataPro[i].discount;
-    category.value = dataPro[i].category;
-    count.style.display="none"
-    getTotal()
-    create.innerHTML = "Update"
-    mood = 'update'
-    index = [i]
-    scroll({
-        top:0, 
-        behavior: "smooth"
-    })
+  title.value = dataPro[i].title;
+  price.value = dataPro[i].price;
+  taxes.value = dataPro[i].taxes;
+  ads.value = dataPro[i].ads;
+  discount.value = dataPro[i].discount;
+  category.value = dataPro[i].category;
+  count.style.display = "none";
+  getTotal();
+  create.innerHTML = "Update";
+  mood = "update";
+  titlePage.innerHTML = "Product | Update";
+
+  index = [i];
+  scroll({
+    top: 0,
+    behavior: "smooth",
+  });
 }
 // search
-let searchMood = 'title'
+let searchMood = "title";
 function getSearchMoode(id) {
-   let search = document.getElementById("search")
-    if (id == 'searchTitle') {
-        searchMood = 'title'
-    } else {
-        searchMood = 'category'
-    }
-    search.focus()
-    search.placeholder = `Search By ${searchMood}`
-    console.log(searchMood)
-    search.value = ""
-    readData()
+  let search = document.getElementById("search");
+  if (id == "searchTitle") {
+    searchMood = "title";
+  } else {
+    searchMood = "category";
+  }
+  search.focus();
+  search.placeholder = `Search By ${searchMood}`;
+  console.log(searchMood);
+  search.value = "";
+  
+  if (searchMood === "title") {
+    titlePage.innerHTML = "Product | Search By Title";
+} else {
+    titlePage.innerHTML = "Product | Search By Category";
+}
+  readData();
 }
 function searchData(value) {
-    if (searchMood == 'title') {
+  if (searchMood == "title") {
+    
+    let table = "";
 
-                    let table = '';
-                for (let i = 0; i < dataPro.length; i++) {
-                    if (dataPro[i].title.includes(value.toLowerCase())) {
-                        console.log(i)
-                            table += `
+    for (let i = 0; i < dataPro.length; i++) {
+      if (dataPro[i].title.includes(value.toLowerCase())) {
+        console.log(i);
+        table += `
                                 <tr>
-                                        <td>${i +1}</td>
+                                        <td>${i + 1}</td>
                                         <td>${dataPro[i].title}</td>
                                         <td>${dataPro[i].price}</td>
                                         <td>${dataPro[i].taxes}</td>
@@ -187,23 +192,27 @@ function searchData(value) {
                                         <td>${dataPro[i].discount}</td>
                                         <td>${dataPro[i].total}$</td>
                                         <td>${dataPro[i].category}</td>
-                                        <td><button onClick="updatePro(${i})">Update</button></td> 
-                                        <td><button onClick="deleteData( ${i} )">Delete</button></td>
+                                        
+                                        <td><button class="updatePtn" onClick="updatePro(${i})"> <i class="fas fa-edit"></i></button></td> 
+                       <td><button class="deletePtn" onClick="deleteData( ${i} )"> <i class="fas fa-trash"></i></button></td>
                                 </tr>
-                                    `
-                                    
-            }}
-            document.getElementById('tbody').innerHTML = table
+                                    `;
+                                }
 
-    } else {
-        let table = '';
+                            }
+                            document.getElementById("tbody").innerHTML = table;
+                            
+                        } 
+                        
+else {
+    let table = "";
 
-            for (let i = 0; i < dataPro.length; i++) {
-                if (dataPro[i].category.includes(value)) {
-                    console.log(i)
-                        table += `
+    for (let i = 0; i < dataPro.length; i++) {
+      if (dataPro[i].category.includes(value)) {
+        console.log(i);
+        table += `
                             <tr>
-                                    <td>${i +1}</td>
+                                    <td>${i + 1}</td>
                                     <td>${dataPro[i].title}</td>
                                     <td>${dataPro[i].price}</td>
                                     <td>${dataPro[i].taxes}</td>
@@ -211,40 +220,43 @@ function searchData(value) {
                                     <td>${dataPro[i].discount}</td>
                                     <td>${dataPro[i].total}$</td>
                                     <td>${dataPro[i].category}</td>
-                                    <td><button onClick="updatePro(${i})">Update</button></td> 
-                                    <td><button onClick="deleteData( ${i} )">Delete</button></td>
+                                    
+                                    <td><button class="updatePtn" onClick="updatePro(${i})"> <i class="fas fa-edit"></i></button></td> 
+                        <td><button class="deletePtn" onClick="deleteData( ${i} )"> <i class="fas fa-trash"></i></button></td>
                             </tr>
-                                `
-                                
-    }}
-    document.getElementById('tbody').innerHTML = table
-
+                                `;
+      }
     }
+    
+    document.getElementById("tbody").innerHTML = table;
+    
 }
+
+}
+
 // clean data
 
 function downloadExcel() {
+  let table = document.getElementById("productTable");
+  let workbook = XLSX.utils.table_to_book(table, { sheet: "Sheet1" });
 
-    let table = document.getElementById('productTable');
-    let workbook = XLSX.utils.table_to_book(table, {sheet: "Sheet1"});
+  let wbout = XLSX.write(workbook, { bookType: "xlsx", type: "binary" });
 
-    let wbout = XLSX.write(workbook, {bookType: 'xlsx', type: 'binary'});
-
-    function s2ab(s) {
-        let buf = new ArrayBuffer(s.length);
-        let view = new Uint8Array(buf);
-        for (let i = 0; i < s.length; i++) {
-            view[i] = s.charCodeAt(i) & 0xFF;
-        }
-        return buf;
+  function s2ab(s) {
+    let buf = new ArrayBuffer(s.length);
+    let view = new Uint8Array(buf);
+    for (let i = 0; i < s.length; i++) {
+      view[i] = s.charCodeAt(i) & 0xff;
     }
+    return buf;
+  }
 
-    let blob = new Blob([s2ab(wbout)], {type: "application/octet-stream"});
-    let url = URL.createObjectURL(blob);
-    let a = document.createElement('a');
-    a.href = url;
-    a.download = 'products.xlsx';
-    a.click();
-    URL.revokeObjectURL(url);
+  let blob = new Blob([s2ab(wbout)], { type: "application/octet-stream" });
+  let url = URL.createObjectURL(blob);
+  let a = document.createElement("a");
+  a.href = url;
+  a.download = "products.xlsx";
+  a.click();
+  URL.revokeObjectURL(url);
 }
 ///////////////////////////
